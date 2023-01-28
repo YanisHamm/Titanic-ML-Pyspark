e4d4796127dcffbf0b3c796db096245f9de52b34
YanisHamm <yyanis20@gmail.com>
2023-01-28 22:24:48 +0100

    .

--- SparkSession.py
+++ SparkSession.py
@@ -1,7 +1,13 @@
 # Starting a Spark session
+from pyspark.sql.functions import count, mean, when, lit, create_map, regexp_extract, col, split
+from itertools import chain
 import pyspark
 import findspark
 findspark.init()
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('Titanic-ML').getOrCreate()
+
+
+train = spark.read.csv('./data/train.csv', header=True, inferSchema=True)
+test  = spark.read.csv('./data/test.csv', header=True, inferSchema=True)
